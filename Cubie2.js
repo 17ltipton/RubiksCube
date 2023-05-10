c9b849e6c3902754add4a2b2a609d9fe0b5565d0
Leon Tipton <58591767+17ltipton@users.noreply.github.com>
2023-05-10 23:11:31 +0100

    Create Cubie2.js

---
+++ Cubie2.js
@@ -0,0 +1,40 @@
+class Cubie {
+  PMatrix3D matrix;
+  int x, y, z = 0;
+  Face[] faces = new Face[6];
+
+  Cubie(PMatrix3D m, int x, int y, int z) {
+    matrix = m;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    
+    faces[0] = new Face(new PVector(0, 0, 1), color(0, 0, 255));
+    faces[1] = new Face(new PVector(0, 0, -1), color(0, 255, 0));
+    faces[2] = new Face(new PVector(0, 1, 0), color(255));
+    faces[3] = new Face(new PVector(0, -1, 0), color(255, 255, 0));
+    faces[4] = new Face(new PVector(-1, 0, 0), color(255, 150, 0));
+    faces[5] = new Face(new PVector(1, 0, 0), color(255, 0, 0));
+  }
+
+  void update(int x, int y, int z) {
+    matrix.reset();
+    matrix.translate(x, y, z);
+    this.x = x;
+    this.y = y;
+    this.z = z;  
+    
+  }
+  
+  void show() {
+    stroke(0);
+    strokeWeight(0.1);
+    pushMatrix();
+    applyMatrix(matrix);
+    box(1);
+    for (Face f: faces){
+      f.show(); 
+    }
+    popMatrix();
+  }
+}
